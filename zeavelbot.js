44b36ddc986539b9611fb5ad05952a3b63ed5c81
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-09-06 14:45:43 +0400

    Update zeavelbot.js

--- zeavelbot.js
+++ zeavelbot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const CC = require('./command_create.js');
 const Command = CC.Command;
-const token = require("./token.json")
+
 
 
 var Commandss = new CC.Commands();
